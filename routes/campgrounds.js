59e9945f583ea0e54078b39225dfd523b1aa4dcc
Syed <szaidi05@gmail.com>
2020-07-19 18:47:04 -0400

    associated user with comments and campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,25 +16,36 @@ router.get("/", function (req, res) {
 });
 
 //CREATE - add new campground to DB
-router.post("/", function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
   //get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = { name: name, image: image, description: desc };
+  var author = {
+    id: req.user._id,
+    username: req.user.username,
+  };
+  var newCampground = {
+    name: name,
+    image: image,
+    description: desc,
+    author: author,
+  };
+
   //Create a new campground and save to DB
   Campground.create(newCampground, function (err, newlyCreated) {
     if (err) {
       console.log(err);
     } else {
       //redirect back to campgrounds page
+      console.log(newlyCreated);
       res.redirect("/campgrounds");
     }
   });
 });
 
 //NEW - show from to create a new campground
-router.get("/new", function (req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
   res.render("campgrounds/new");
 });
 
@@ -52,5 +63,12 @@ router.get("/:id", function (req, res) {
       }
     });
 });
+//Middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
