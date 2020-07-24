e9c56e57e9d00467b5538542f7d304e506066180
Syed <szaidi05@gmail.com>
2020-07-24 14:21:56 -0400

    add start script

--- app.js
+++ app.js
@@ -57,7 +57,11 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 //Server setup
-
-app.listen("3000", function () {
-  console.log("The YelpCamp Server Has Started!");
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
 });
+
+// app.listen("3000", function () {
+//   console.log("The YelpCamp Server Has Started!");
+// });
